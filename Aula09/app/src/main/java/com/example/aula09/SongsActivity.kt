babe281f692814d4a3b8355521aa3fb0cedf81de
Arthur Paulucci Carnieto <arthurcarnieto@gmail.com>
2022-01-31 03:01:38 -0300

    Add and configure recyclerView on main activity
    A very simple Toast is showed when you click in a song.

--- Aula09/app/src/main/java/com/example/aula09/SongsActivity.kt
+++ Aula09/app/src/main/java/com/example/aula09/SongsActivity.kt
@@ -2,10 +2,23 @@ package com.example.aula09
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SongsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_songs)
+
+        val onClickListener = ItemClickListener { song ->
+            Toast.makeText(this, "'${song.name}' clicked", Toast.LENGTH_SHORT).show()
+        }
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val adapter = SongsAdapter(SongsData().loadItems(), onClickListener)
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
     }
 }
