c18b68a51efd9bf7089a6e610b5a14fb5a1519e6
Arthur Paulucci Carnieto <arthurcarnieto@gmail.com>
2022-02-05 20:18:07 -0300

    Create RelativeLayout layout

--- Aula04/app/src/main/java/com/example/aula04/MainActivity.kt
+++ Aula04/app/src/main/java/com/example/aula04/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_linear_layout)
+        setContentView(R.layout.activity_relative_layout)
     }
 }
