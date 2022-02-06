21db363aee40b9e75472be16a06c4f7040538eed
Arthur Paulucci Carnieto <arthurcarnieto@gmail.com>
2022-02-06 19:55:42 -0300

    Create a basic AlertDialog on main activity

--- Aula12/app/src/main/java/com/example/aula12/MainActivity.kt
+++ Aula12/app/src/main/java/com/example/aula12/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.aula12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import android.content.DialogInterface
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // reference: https://developer.android.com/guide/topics/ui/dialogs?hl=pt-br
+        val alertDialog: AlertDialog = this.let {
+            val builder = AlertDialog.Builder(it)
+            builder.apply {
+                setTitle(R.string.alert_dialog_title)
+                setMessage(R.string.alert_dialog_message)
+                setPositiveButton(R.string.alert_dialog_ok) { dialog, id ->
+                    // User clicked OK button
+                }
+            }
+            // Create the AlertDialog
+            builder.create()
+        }
+
+        alertDialog.show()
     }
 }
